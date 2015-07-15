535d28d1b0632804ea0e11f2ef250529f6464bc9
Sebastian Suttner <sebastian.suttner@gmail.com>
2015-07-15 00:48:39 -0300

    Calculate numbers

--- numbers.rb
+++ numbers.rb
@@ -1,3 +1,15 @@
 module Number
 
+  def self.one
+    1
+  end
+
+  def self.two
+    2
+  end
+
+  def self.three
+    3
+  end
+
 end
--- spec/numbers_spec.rb
+++ spec/numbers_spec.rb
@@ -2,4 +2,16 @@ require_relative '../numbers'
 
 describe Number do
 
+  describe '.one' do
+    it { expect(Number.one).to eq(1) }
+  end
+
+  describe '.two' do
+    it { expect(Number.two).to eq(2) }
+  end
+
+  describe '.three' do
+    it { expect(Number.three).to eq(3) }
+  end
+
 end
